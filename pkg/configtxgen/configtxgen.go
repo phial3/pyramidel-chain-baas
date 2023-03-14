c62963aa5fadebdc951a5ce1b876cfd4794713af
hxx258456 <13011715880@163.com>
2023-03-14 18:02:01 +0800

    refactor: add configtx config
    confitxgen config struct

--- pkg/configtxgen/configtxgen.go
+++ pkg/configtxgen/configtxgen.go
@@ -1,163 +1,205 @@
 package configtxgen
 
-//
-//type TopLevel struct {
-//	Organizations []Organizations `yaml:"Organizations"`
-//	Capabilities  Capabilities    `yaml:"Capabilities"`
-//	Application   Application     `yaml:"Application"`
-//	Orderer       Orderer         `yaml:"Orderer"`
-//	Channel       Channel         `yaml:"Channel"`
-//	Profiles      Profiles        `yaml:"Profiles"`
-//}
-//type Readers struct {
-//	Type string `yaml:"Type"`
-//	Rule string `yaml:"Rule"`
-//}
-//type Writers struct {
-//	Type string `yaml:"Type"`
-//	Rule string `yaml:"Rule"`
-//}
-//type Admins struct {
-//	Type string `yaml:"Type"`
-//	Rule string `yaml:"Rule"`
-//}
-//type Endorsement struct {
-//	Type string `yaml:"Type"`
-//	Rule string `yaml:"Rule"`
-//}
-//type Policies struct {
-//	Readers     Readers     `yaml:"Readers"`
-//	Writers     Writers     `yaml:"Writers"`
-//	Admins      Admins      `yaml:"Admins"`
-//	Endorsement Endorsement `yaml:"Endorsement"`
-//}
-//type AnchorPeers struct {
-//	Host string `yaml:"Host"`
-//	Port int    `yaml:"Port"`
-//}
-//type Organizations struct {
-//	Name             string        `yaml:"Name"`
-//	ID               string        `yaml:"ID"`
-//	MSPDir           string        `yaml:"MSPDir"`
-//	Policies         Policies      `yaml:"Policies"`
-//	AnchorPeers      []AnchorPeers `yaml:"AnchorPeers"`
-//	OrdererEndpoints []string      `yaml:"OrdererEndpoints"`
-//}
-//type Channel struct {
-//	V20 bool `yaml:"V2_0"`
-//}
-//type Orderer struct {
-//	V20 bool `yaml:"V2_0"`
-//}
-//type Application struct {
-//	V20 bool `yaml:"V2_0"`
-//}
-//type Capabilities struct {
-//	Channel     Channel     `yaml:"Channel"`
-//	Orderer     Orderer     `yaml:"Orderer"`
-//	Application Application `yaml:"Application"`
-//}
-//type LifecycleEndorsement struct {
-//	Type string `yaml:"Type"`
-//	Rule string `yaml:"Rule"`
-//}
-//type Policies struct {
-//	Readers              Readers              `yaml:"Readers"`
-//	Writers              Writers              `yaml:"Writers"`
-//	Admins               Admins               `yaml:"Admins"`
-//	LifecycleEndorsement LifecycleEndorsement `yaml:"LifecycleEndorsement"`
-//	Endorsement          Endorsement          `yaml:"Endorsement"`
-//}
-//type Capabilities struct {
-//	V20 bool `yaml:"V2_0"`
-//}
-//type Application struct {
-//	Organizations interface{}  `yaml:"Organizations"`
-//	Policies      Policies     `yaml:"Policies"`
-//	Capabilities  Capabilities `yaml:"Capabilities"`
-//}
-//type Consenters struct {
-//	Host          string `yaml:"Host"`
-//	Port          int    `yaml:"Port"`
-//	ClientTLSCert string `yaml:"ClientTLSCert"`
-//	ServerTLSCert string `yaml:"ServerTLSCert"`
-//}
-//type EtcdRaft struct {
-//	Consenters []Consenters `yaml:"Consenters"`
-//}
-//type BatchSize struct {
-//	MaxMessageCount   int    `yaml:"MaxMessageCount"`
-//	AbsoluteMaxBytes  string `yaml:"AbsoluteMaxBytes"`
-//	PreferredMaxBytes string `yaml:"PreferredMaxBytes"`
-//}
-//type BlockValidation struct {
-//	Type string `yaml:"Type"`
-//	Rule string `yaml:"Rule"`
-//}
-//type Policies struct {
-//	Readers         Readers         `yaml:"Readers"`
-//	Writers         Writers         `yaml:"Writers"`
-//	Admins          Admins          `yaml:"Admins"`
-//	BlockValidation BlockValidation `yaml:"BlockValidation"`
-//}
-//type Orderer struct {
-//	OrdererType   string      `yaml:"OrdererType"`
-//	Addresses     []string    `yaml:"Addresses"`
-//	EtcdRaft      EtcdRaft    `yaml:"EtcdRaft"`
-//	BatchTimeout  string      `yaml:"BatchTimeout"`
-//	BatchSize     BatchSize   `yaml:"BatchSize"`
-//	Organizations interface{} `yaml:"Organizations"`
-//	Policies      Policies    `yaml:"Policies"`
-//}
-//type Policies struct {
-//	Readers Readers `yaml:"Readers"`
-//	Writers Writers `yaml:"Writers"`
-//	Admins  Admins  `yaml:"Admins"`
-//}
-//type Channel struct {
-//	Policies     Policies     `yaml:"Policies"`
-//	Capabilities Capabilities `yaml:"Capabilities"`
-//}
-//type Policies struct {
-//	Readers         Readers         `yaml:"Readers"`
-//	Writers         Writers         `yaml:"Writers"`
-//	Admins          Admins          `yaml:"Admins"`
-//	BlockValidation BlockValidation `yaml:"BlockValidation"`
-//}
-//type Orderer struct {
-//	OrdererType   string          `yaml:"OrdererType"`
-//	Addresses     []string        `yaml:"Addresses"`
-//	EtcdRaft      EtcdRaft        `yaml:"EtcdRaft"`
-//	BatchTimeout  string          `yaml:"BatchTimeout"`
-//	BatchSize     BatchSize       `yaml:"BatchSize"`
-//	Organizations []Organizations `yaml:"Organizations"`
-//	Policies      Policies        `yaml:"Policies"`
-//	Capabilities  Capabilities    `yaml:"Capabilities"`
-//}
-//type SampleConsortium struct {
-//	Organizations []Organizations `yaml:"Organizations"`
-//}
-//type Consortiums struct {
-//	SampleConsortium SampleConsortium `yaml:"SampleConsortium"`
-//}
-//type ThreeOrgsOrdererGenesis struct {
-//	Policies     Policies     `yaml:"Policies"`
-//	Capabilities Capabilities `yaml:"Capabilities"`
-//	Orderer      Orderer      `yaml:"Orderer"`
-//	Consortiums  Consortiums  `yaml:"Consortiums"`
-//}
-//type Policies struct {
-//	Readers Readers `yaml:"Readers"`
-//	Writers Writers `yaml:"Writers"`
-//	Admins  Admins  `yaml:"Admins"`
-//}
-//type ThreeOrgsChannel struct {
-//	Consortium   string       `yaml:"Consortium"`
-//	Policies     Policies     `yaml:"Policies"`
-//	Capabilities Capabilities `yaml:"Capabilities"`
-//	Application  Application  `yaml:"Application"`
-//}
-//type Profiles struct {
-//	ThreeOrgsOrdererGenesis ThreeOrgsOrdererGenesis `yaml:"ThreeOrgsOrdererGenesis"`
-//	ThreeOrgsChannel        ThreeOrgsChannel        `yaml:"ThreeOrgsChannel"`
-//}
+import "time"
+
+const (
+	// The type key for etcd based RAFT consensus.
+	EtcdRaft = "etcdraft"
+)
+
+const (
+	// SampleInsecureSoloProfile references the sample profile which does not
+	// include any MSPs and uses solo for ordering.
+	SampleInsecureSoloProfile = "SampleInsecureSolo"
+	// SampleDevModeSoloProfile references the sample profile which requires
+	// only basic membership for admin privileges and uses solo for ordering.
+	SampleDevModeSoloProfile = "SampleDevModeSolo"
+	// SampleSingleMSPSoloProfile references the sample profile which includes
+	// only the sample MSP and uses solo for ordering.
+	SampleSingleMSPSoloProfile = "SampleSingleMSPSolo"
+
+	// SampleInsecureKafkaProfile references the sample profile which does not
+	// include any MSPs and uses Kafka for ordering.
+	SampleInsecureKafkaProfile = "SampleInsecureKafka"
+	// SampleDevModeKafkaProfile references the sample profile which requires only
+	// basic membership for admin privileges and uses Kafka for ordering.
+	SampleDevModeKafkaProfile = "SampleDevModeKafka"
+	// SampleSingleMSPKafkaProfile references the sample profile which includes
+	// only the sample MSP and uses Kafka for ordering.
+	SampleSingleMSPKafkaProfile = "SampleSingleMSPKafka"
+
+	// SampleDevModeEtcdRaftProfile references the sample profile used for testing
+	// the etcd/raft-based ordering service.
+	SampleDevModeEtcdRaftProfile = "SampleDevModeEtcdRaft"
+
+	// SampleSingleMSPChannelProfile references the sample profile which
+	// includes only the sample MSP and is used to create a channel
+	SampleSingleMSPChannelProfile = "SampleSingleMSPChannel"
+
+	// SampleConsortiumName is the sample consortium from the
+	// sample configtx.yaml
+	SampleConsortiumName = "SampleConsortium"
+	// SampleOrgName is the name of the sample org in the sample profiles
+	SampleOrgName = "SampleOrg"
+
+	// AdminRoleAdminPrincipal is set as AdminRole to cause the MSP role of
+	// type Admin to be used as the admin principal default
+	AdminRoleAdminPrincipal = "Role.ADMIN"
+)
+
+// TopLevel consists of the structs used by the configtxgen tool.
+type TopLevel struct {
+	Profiles      map[string]*Profile        `yaml:"Profiles" json:"profiles,omitempty"`
+	Organizations []*Organization            `yaml:"Organizations" json:"organizations,omitempty"`
+	Channel       *Profile                   `yaml:"Channel" json:"channel,omitempty"`
+	Application   *Application               `yaml:"Application" json:"application,omitempty"`
+	Orderer       *Orderer                   `yaml:"Orderer" json:"orderer,omitempty"`
+	Capabilities  map[string]map[string]bool `yaml:"Capabilities" json:"capabilities,omitempty"`
+}
+
+// Profile encodes orderer/application configuration combinations for the
+// configtxgen tool.
+type Profile struct {
+	Consortium   string                 `yaml:"Consortium"`
+	Application  *Application           `yaml:"Application"`
+	Orderer      *Orderer               `yaml:"Orderer"`
+	Consortiums  map[string]*Consortium `yaml:"Consortiums"`
+	Capabilities map[string]bool        `yaml:"Capabilities"`
+	Policies     map[string]*Policy     `yaml:"Policies"`
+}
+
+// Policy encodes a channel config policy
+type Policy struct {
+	Type string `yaml:"Type"`
+	Rule string `yaml:"Rule"`
+}
+
+// Consortium represents a group of organizations which may create channels
+// with each other
+type Consortium struct {
+	Organizations []*Organization `yaml:"Organizations"`
+}
+
+// Application encodes the application-level configuration needed in config
+// transactions.
+type Application struct {
+	Organizations []*Organization    `yaml:"Organizations"`
+	Capabilities  map[string]bool    `yaml:"Capabilities"`
+	Policies      map[string]*Policy `yaml:"Policies"`
+	ACLs          map[string]string  `yaml:"ACLs"`
+}
+
+// Organization encodes the organization-level configuration needed in
+// config transactions.
+type Organization struct {
+	Name     string             `yaml:"Name"`
+	ID       string             `yaml:"ID"`
+	MSPDir   string             `yaml:"MSPDir"`
+	MSPType  string             `yaml:"MSPType"`
+	Policies map[string]*Policy `yaml:"Policies"`
+
+	// Note: Viper deserialization does not seem to care for
+	// embedding of types, so we use one organization struct
+	// for both orderers and applications.
+	AnchorPeers      []*AnchorPeer `yaml:"AnchorPeers"`
+	OrdererEndpoints []string      `yaml:"OrdererEndpoints"`
+
+	// SkipAsForeign indicates that this org definition is actually unknown to this
+	// instance of the tool, so, parsing of this org's parameters should be ignored.
+	SkipAsForeign bool
+}
+
+// AnchorPeer encodes the necessary fields to identify an anchor peer.
+type AnchorPeer struct {
+	Host string `yaml:"Host"`
+	Port int    `yaml:"Port"`
+}
+
+// Orderer contains configuration associated to a channel.
+type Orderer struct {
+	OrdererType   string             `yaml:"OrdererType"`
+	Addresses     []string           `yaml:"Addresses"`
+	BatchTimeout  time.Duration      `yaml:"BatchTimeout"`
+	BatchSize     BatchSize          `yaml:"BatchSize"`
+	Kafka         Kafka              `yaml:"Kafka"`
+	EtcdRaft      *ConfigMetadata    `yaml:"EtcdRaft"`
+	Organizations []*Organization    `yaml:"Organizations"`
+	MaxChannels   uint64             `yaml:"MaxChannels"`
+	Capabilities  map[string]bool    `yaml:"Capabilities"`
+	Policies      map[string]*Policy `yaml:"Policies"`
+}
+
+// BatchSize contains configuration affecting the size of batches.
+type BatchSize struct {
+	MaxMessageCount   uint32 `yaml:"MaxMessageCount"`
+	AbsoluteMaxBytes  uint32 `yaml:"AbsoluteMaxBytes"`
+	PreferredMaxBytes uint32 `yaml:"PreferredMaxBytes"`
+}
+
+// Kafka contains configuration for the Kafka-based orderer.
+type Kafka struct {
+	Brokers []string `yaml:"Brokers"`
+}
+
+var genesisDefaults = TopLevel{
+	Orderer: &Orderer{
+		OrdererType:  "solo",
+		BatchTimeout: 2 * time.Second,
+		BatchSize: BatchSize{
+			MaxMessageCount:   500,
+			AbsoluteMaxBytes:  10 * 1024 * 1024,
+			PreferredMaxBytes: 2 * 1024 * 1024,
+		},
+		Kafka: Kafka{
+			Brokers: []string{"127.0.0.1:9092"},
+		},
+		EtcdRaft: &ConfigMetadata{
+			Options: &Options{
+				TickInterval:         "500ms",
+				ElectionTick:         10,
+				HeartbeatTick:        1,
+				MaxInflightBlocks:    5,
+				SnapshotIntervalSize: 16 * 1024 * 1024, // 16 MB
+			},
+		},
+	},
+}
+
+// ConfigMetadata is serialized and set as the value of ConsensusType.Metadata in
+// a channel configuration when the ConsensusType.Type is set "etcdraft".
+type ConfigMetadata struct {
+	Consenters           []*Consenter `json:"consenters,omitempty"`
+	Options              *Options     `json:"options,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
+	XXX_unrecognized     []byte       `json:"-"`
+	XXX_sizecache        int32        `json:"-"`
+}
+
+// Consenter represents a consenting node (i.e. replica).
+type Consenter struct {
+	Host                 string   `json:"host,omitempty"`
+	Port                 uint32   `json:"port,omitempty"`
+	ClientTlsCert        []byte   `json:"client_tls_cert,omitempty"`
+	ServerTlsCert        []byte   `json:"server_tls_cert,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+// Options to be specified for all the etcd/raft nodes. These can be modified on a
+// per-channel basis.
+type Options struct {
+	TickInterval      string `json:"tick_interval,omitempty"`
+	ElectionTick      uint32 `json:"election_tick,omitempty"`
+	HeartbeatTick     uint32 `json:"heartbeat_tick,omitempty"`
+	MaxInflightBlocks uint32 `json:"max_inflight_blocks,omitempty"`
+	// Take snapshot when cumulative data exceeds certain size in bytes.
+	SnapshotIntervalSize uint32   `json:"snapshot_interval_size,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func GenConfigtxConfig() TopLevel {
+	return TopLevel{}
+}
